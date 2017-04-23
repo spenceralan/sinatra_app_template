ce1d7505326c0359bc93980c1661c356681f6604
Spencer Alan <spencer.alan@icloud.com>
2017-04-23 10:20:19 -0700

    add example code to ruby and spec files

--- sinatra_builder.rb
+++ sinatra_builder.rb
@@ -107,12 +107,58 @@ Copyright © 2017 <!--YOUR NAME HERE-->
 TEXT
 
     write_contents lib_path, "#{project_name}.rb", <<-TEXT
-# backend ruby code goes here
+# example ruby code
+
+# class Palindrome
+
+#   def is_word?(user_input)
+#     user_input.match?(/[aeiouy]+/i)
+#   end
+
+# end
 TEXT
 
-    write_contents spec_path, "#{project_name}_spec.rb"
+    write_contents spec_path, "#{project_name}_spec.rb", <<-TEXT
+require "#{project_name}"
+require "rspec"
+require "pry"
 
-    write_contents spec_path, "#{project_name}_integration_spec.rb"
+
+# example project spec
+
+# describe 'Palindrome#isWord?' do
+#   let(:word) { Palindrome.new }
+
+#   it "returns true if the word has at least one vowel" do
+#     expect(word.is_word?("word")).to eq true
+#   end
+
+#   it "returns false if the word does not have a vowel" do
+#     expect(word.is_word?("wrd")).to eq false
+#   end
+
+# end
+TEXT
+
+    write_contents spec_path, "#{project_name}_integration_spec.rb", <<-TEXT
+require "capybara/rspec"
+require "./app"
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+# example integration test
+
+# describe("the phrase parser path", {:type => :feature}) do
+#   it("processes the user input and returns correct message if its a palindrome") do
+#     visit("/")
+#     fill_in("phrase1", :with => "madam")
+#     fill_in("phrase2", :with => "anagram")
+#     click_button("what am i?")
+#     expect(page).to have_content("'madam' is a palindrome")
+#   end
+# end
+TEXT
 
     write_contents views_path, "index.erb"
 
