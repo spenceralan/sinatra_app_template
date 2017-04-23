b6336cc9653b80d410f3adfa1c2c00b3414718ce
Spencer Alan <spencer.alan@icloud.com>
2017-04-23 09:15:46 -0700

    add contents for readme

--- sinatra_builder.rb
+++ sinatra_builder.rb
@@ -64,7 +64,47 @@ gem "capybara"
 gem "pry"
 TEXT
 
-    write_contents project_path, "README.md"
+    write_contents project_path, "README.md", <<-TEXT
+# <!--PROJECT NAME HERE-->
+
+<!--PROJECT DESCRIPTION HERE-->
+
+### Prerequisites
+
+Web browser with ES6 compatibility
+Examples: Chrome, Safari
+
+Ruby <!--VERSION HERE-->
+Bundler
+
+### Installing
+
+Installation is quick and easy! First you can open this link <!--HEROKU LINK HERE--> to see the webpage in action live online. Or you can clone this repository to your machine, navigate to the file path in your terminal, and run 'app.rb' by typing '$ruby app.rb'. If you chose to clone the repository, after you run 'app.rb' you will need to copy the localhost path into your web browswer. The standard localhost for Sinatra is port 4567
+
+## Built With
+
+* Ruby
+* Sinatra
+* HTML
+* CSS
+* Bootstrap https://getbootstrap.com/
+* ES6
+* Jquery https://jquery.com/
+
+## Specifications
+
+| behavior |  input   |  output  |
+|----------|:--------:|:--------:|
+<!--SPECS GO HERE-->
+ 
+## Authors
+
+* <!--YOUR NAME HERE-->
+
+## License
+
+Copyright © 2017 <!--YOUR NAME HERE-->
+TEXT
 
     write_contents lib_path, "#{project_name}.rb"
 
