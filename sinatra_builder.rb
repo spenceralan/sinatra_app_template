32168e8c8b506be0efe09addb6dfe3577c3f51e8
Spencer Alan <spencer.alan@icloud.com>
2017-04-22 19:52:01 -0700

    add method to build files with contents

--- sinatra_builder.rb
+++ sinatra_builder.rb
@@ -9,9 +9,34 @@ project = "project" if project == ""
 
 puts "Building #{project}........"
 
-PROJECT_PATH = File.expand_path(File.join("~", "Desktop", project))
+project_path = File.expand_path(File.join("~", "Desktop", project))
+lib_path = File.join(project_path, "lib")
+spec_path = File.join(project_path, "spec")
+views_path = File.join(project_path, "views")
+css_path = File.join(project_path, "public/css")
+img_path = File.join(project_path, "public/img")
+js_path = File.join(project_path, "public/js")
 
-["lib", "spec", "views", "public/css", "public/img", "public/js"].each do |filename|
-  FileUtils.mkdir_p(File.join(PROJECT_PATH, filename))
+[lib_path, spec_path, views_path, css_path, img_path, js_path].each do |folder|
+  FileUtils.mkdir_p(folder)
 end
+
+def write_file(path, filename, contents)
+  File.open(File.join(path, filename), "w") do |f|
+    f.write(contents)
+  end
+end
+
+write_file project_path, "app.rb", <<-TEXT
+require 'sinatra'
+require 'sinatra/reloader'
+require './lib/#{project}'
+require 'pry'
+
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+TEXT
 
